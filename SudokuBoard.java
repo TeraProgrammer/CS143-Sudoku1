24d4674cc7825da8caadd1994ed4f73a3a5e240d
TeraProgrammer <124451927+TeraProgrammer@users.noreply.github.com>
2025-04-20 23:33:00 -0700

    Try & catch statements added as well as Scanners
    Scanners used to scan the given .sdk file line-by-line. Accounted for FileNotFoundException error.

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,11 +1,25 @@
+import java.util.*;
+import java.io.*;
+
 public class SudokuBoard{
-   private char[][] board;
+   private char[][] board = new char[9][9];
    
-   public SudokuBoard(String fileName){
-      
+   public SudokuBoard(String fileName) {
+      try {
+         Scanner fileIn = new Scanner(new File(fileName));
+        
+         for (int r = 0; r < board.length; r++) {
+            String line = fileIn.nextLine();
+            for (int c = 0; c < board[0].length; c++){
+               board[r][c] = line.charAt(c);
+            }
+         }
+      } catch (FileNotFoundException e) {
+         System.out.println("Unknown name, file not found.");
+      }
    }
    
    public String toString(){
-   
+      return "";
    }
 }
